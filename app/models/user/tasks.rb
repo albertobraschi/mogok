fb732aebcfc5f0003ec26433e6704a920597e68e
unknown <mogoktracker@gmail.com>
2009-06-07 10:38:12 -0300

    perpetual ticket

--- app/models/user/tasks.rb
+++ app/models/user/tasks.rb
@@ -8,7 +8,7 @@ class User
     absents = find :all, :conditions => ['last_request_at < ? AND active = TRUE', threshold]
 
     absents.each do |u|
-      next if u.has_ticket?(:staff) || u.has_ticket?(:inactivity_exempt)
+      next if u.has_ticket?(:staff) || u.has_ticket?(:perpetual)
       if u.torrents.count > 0
         u.inactivate # inactivate if user has at least one torrent
         Log.create "User #{u.username} inactivated by system (unused account)."
